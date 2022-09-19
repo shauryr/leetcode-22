6cf8ffd0d86004b77096acdde00bdd7a6831de3a
Shaurya Rohatgi <shauryr@gmail.com>
2022-09-19 16:58:07 -0400

    Time: 67 ms (13.42%), Space: 13.9 MB (26.58%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,19 +1,34 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
         
-        mapping_braces = {
+        """
+        create a map of reverse paranthesis(P)
+        create a stack
+        iterate over s:
+            if encountered P is in the map:
+                pop from the stack
+                if popped element is not equal to map value
+                return false
+            else:
+                append(stack)
+        
+        """
+        
+        map_para = {
+            ']':'[',
             ')' : '(',
-            "}" : "{",
-            "]" : "["
+            '}' : '{'
         }
         
-        for c in s:
-            if c in mapping_braces and len(stack)!=0 :
-                top = stack.pop()
-                if top != mapping_braces[c]:
+        stack = []
+        
+        for p in s:
+            if p in map_para and stack:
+                p_rev = stack.pop()
+                if p_rev != map_para[p]:
                     return False
             else:
-                stack.append(c)
+                stack.append(p)
         
         return not stack
+                
