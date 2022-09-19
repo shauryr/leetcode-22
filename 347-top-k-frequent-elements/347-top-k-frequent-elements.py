937ffee570bc496314bb673c20e9729c106260bc
Shaurya Rohatgi <shauryr@gmail.com>
2022-09-19 15:27:59 -0400

    Time: 221 ms (23.77%), Space: 18.8 MB (48.13%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -0,0 +1,24 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        from collections import Counter
+        # n log k
+        # find count of all the numbers
+        # create a max heap of size n
+        # add all elements
+        # pop the heap n times to return element
+        dict_counts = Counter(nums)
+        
+        tups = []
+        heap = []
+        for value, count in dict_counts.items():
+            tups.append((-count, value))
+            
+        for pair in tups:
+            heappush(heap, pair)
+        
+        result = []
+        for i in range(k):
+            result.append(heappop(heap)[1])
+        
+        return result
+            
